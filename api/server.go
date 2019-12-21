a9710bb44024c06b32995f94511cd5d8af26055c
Diana Esteves <desteves89@gmail.com>
2019-12-21 13:08:44 -0600

    fix everything to compile with refactor
    Signed-off-by: Peter Blum <peter.blum@zapatacomputing.com>

--- api/server.go
+++ api/server.go
@@ -1,47 +1,47 @@
 package api
 
-import (
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
+// import (
+// 	"net/http"
+// 	"os"
+// 	"os/signal"
+// 	"time"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/oskoss/mi-casa/home"
-	log "github.com/sirupsen/logrus"
-)
+// 	"github.com/go-chi/chi"
+// 	"github.com/go-chi/chi/middleware"
+// 	"github.com/oskoss/mi-casa/home"
+// 	log "github.com/sirupsen/logrus"
+// )
 
-func Start(port string, myHome *home.Home) {
+// func Start(port string, myHome *home.Home) {
 
-	router := chi.NewRouter()
-	router.Use(middleware.RequestID)
-	router.Use(middleware.RealIP)
-	router.Use(middleware.Logger)
-	router.Use(middleware.Recoverer)
-	router.Use(middleware.Timeout(60 * time.Second))
-	router.Route("/v1/hvac", func(router chi.Router) {
-		//TODO ADD STATUS
-		// router.Get("/status", handleV1HVACStatus)
-		router.Post("/temperature", handleV1HVACTemperature(myHome))
-	})
-	webServer := &http.Server{
-		Addr:         "0.0.0.0:" + port,
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		IdleTimeout:  time.Second * 60,
-		Handler:      router,
-	}
-	go func() {
-		if err := webServer.ListenAndServe(); err != nil {
-			log.WithFields(log.Fields{
-				"err": err,
-			}).Fatalf("server crashed")
-		}
-	}()
+// 	router := chi.NewRouter()
+// 	router.Use(middleware.RequestID)
+// 	router.Use(middleware.RealIP)
+// 	router.Use(middleware.Logger)
+// 	router.Use(middleware.Recoverer)
+// 	router.Use(middleware.Timeout(60 * time.Second))
+// 	router.Route("/v1/hvac", func(router chi.Router) {
+// 		//TODO ADD STATUS
+// 		// router.Get("/status", handleV1HVACStatus)
+// 		router.Post("/temperature", handleV1HVACTemperature(myHome))
+// 	})
+// 	webServer := &http.Server{
+// 		Addr:         "0.0.0.0:" + port,
+// 		WriteTimeout: time.Second * 15,
+// 		ReadTimeout:  time.Second * 15,
+// 		IdleTimeout:  time.Second * 60,
+// 		Handler:      router,
+// 	}
+// 	go func() {
+// 		if err := webServer.ListenAndServe(); err != nil {
+// 			log.WithFields(log.Fields{
+// 				"err": err,
+// 			}).Fatalf("server crashed")
+// 		}
+// 	}()
 
-	channel := make(chan os.Signal, 1)
-	signal.Notify(channel, os.Interrupt)
+// 	channel := make(chan os.Signal, 1)
+// 	signal.Notify(channel, os.Interrupt)
 
-	<-channel
-}
+// 	<-channel
+// }
